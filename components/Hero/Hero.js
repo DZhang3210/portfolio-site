bcd2f38925c963818d7b293806d26b6aaf24fa93
DZhang3210 <53838139+DZhang3210@users.noreply.github.com>
2024-08-10 19:07:56 -0400

    new commit

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,26 +1,32 @@
-'use client'
-import React, { useState } from 'react'
-import { FlipWords } from '../ui/flip-words'
-import {motion} from 'framer-motion'
+"use client";
+import React, { useState } from "react";
+import { FlipWords } from "../ui/flip-words";
+import { motion } from "framer-motion";
 
 const Hero = () => {
-  const [title, setTitle] = useState(0)
-  const words = ['Frontend Developer', 'Backend Developer', 'Fullstack Engineer']
+  const [title, setTitle] = useState(0);
+  const words = [
+    "Frontend Developer",
+    "Backend Developer",
+    "Fullstack Engineer",
+  ];
   return (
     <div>
-        <div className='text-8xl sm:text-9xl'>
-        David Zhang 
-        </div>
-        <motion.div 
-            className='text-5xl sm:text-6xl'
-            initial = {{y:10, opacity:0}}
-            animate = {{y:0, opacity:1,transition:{delay:0.4, duration:0.2}}}
-        >
-            {/* <FlipWords words={words} />  */}
-            Frontend Engineer
-        </motion.div>
+      <div className="text-8xl sm:text-9xl">David Zhang</div>
+      <motion.div
+        className="text-5xl sm:text-6xl"
+        initial={{ y: 10, opacity: 0 }}
+        animate={{
+          y: 0,
+          opacity: 1,
+          transition: { delay: 0.4, duration: 0.2 },
+        }}
+      >
+        {/* <FlipWords words={words} />  */}
+        Fullstack Engineer
+      </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- components/Hero/RecentProjects.js
+++ components/Hero/RecentProjects.js
@@ -1,60 +1,85 @@
-import { Github, Link } from 'lucide-react'
-import Image from 'next/image'
-import React from 'react'
+import { Github, Link } from "lucide-react";
+import Image from "next/image";
+import React from "react";
 
 const RecentProjects = () => {
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 grid-rows-2 gap-4'>
-        {[["/pathfinder_demo.gif","Pathfinding Visualizer", "App to Demonstrate Live Pathfinding Algorithms", "http://pathvis.s3-website-us-east-1.amazonaws.com", "https://github.com/DZhang3210/Pathfinder"],
-        // ["/BugBuddy-gif.gif","Bug Buddy", "Live Bug Tracking and Documentation App", "https://bug-frontend-hnc8.onrender.com", "https://github.com/DZhang3210/Bug-Buddy"],
-        ['/MagicPrompt-Gif.gif', 'MagicPrompt', 'Create, Like, Filter, and Share all your favorite AI Prompts to a likeminded community', 'https://project-promptopia.netlify.app/','https://github.com/DZhang3210/project_promptopia'],
-        ["/Upwork-gif.gif", "OfficeVibe", "Mobile-responsive front-end interface replicating the OfficeVibe platform", "https://workvibe.netlify.app/", "https://github.com/DZhang3210/work-vibe"],
-        ["/Unsplash-gif.gif", "Unsplash", "Mobile-responsive front-end interface replicating the Unsplash platform", "https://unsplash-replica1.netlify.app/", "https://github.com/DZhang3210/unsplash"]
-        ].map(([preview, title, desc, link, github],i)=>(
-            <div 
-                className='flex flex-col border rounded-2xl p-5 border-secondary gap-4 transition hover:-translate-y-1 cursor-pointer'
-                key = {i}
-            >
-                <Image
-                    src = {preview}
-                    alt = {preview}
-                    width = {100}
-                    height = {50}
-                    className='w-full'
-                />
-                <div>
-                    <div className='text-xl font-semibold'>
-                        {title}
-                    </div>
-                    <p>
-                        {desc}
-                    </p>
-                </div>
-                <div className='flex pr-5 gap-2 text-white'>
-                        <a
-                            className='flex gap-1 p-1 bg-[#2C3E50] transition hover:text-blue-400 rounded-lg'
-                            href = {link}
-                            target="_blank"
-                        >
-                            <Link/>
-                            Link
-                        </a>
-                        <a
-                            className='flex gap-2 p-1 bg-[#333333] transition hover:text-blue-400 rounded-lg'
-                            href = {github}
-                            target="_blank"
-                        >
-                            <Github/>
-                            Github
-                        </a>
-                </div>
-            </div>
-        ))
-        
-        }
-      
-    </div>
-  )
-}
+    <div className="grid grid-cols-1 sm:grid-cols-2 grid-rows-2 gap-4">
+      {[
+        [
+          "/eCommerce.gif",
+          "eCommerce",
+          "Mobile-responsive eCommerce marketiplace utilizing postgresql and authenticated Users + Payment",
+          "https://prisma-deploy-kohl.vercel.app/",
+          "https://github.com/DZhang3210/prisma-deploy",
+        ],
 
-export default RecentProjects
+        [
+          "/MagicPrompt-Gif.gif",
+          "MagicPrompt",
+          "Create, Like, Filter, and Share all your favorite AI Prompts to a likeminded community",
+          "https://project-promptopia.netlify.app/",
+          "https://github.com/DZhang3210/project_promptopia",
+        ],
+        [
+          "/Upwork-gif.gif",
+          "OfficeVibe",
+          "Mobile-responsive front-end interface replicating the OfficeVibe platform",
+          "https://workvibe.netlify.app/",
+          "https://github.com/DZhang3210/work-vibe",
+        ],
+        [
+          "/Unsplash-gif.gif",
+          "Unsplash",
+          "Mobile-responsive front-end interface replicating the Unsplash platform",
+          "https://unsplash-replica1.netlify.app/",
+          "https://github.com/DZhang3210/unsplash",
+        ],
+        [
+          "/pathfinder_demo.gif",
+          "Pathfinding Visualizer",
+          "App to Demonstrate Live Pathfinding Algorithms",
+          "http://pathvis.s3-website-us-east-1.amazonaws.com",
+          "https://github.com/DZhang3210/Pathfinder",
+        ],
+      ].map(([preview, title, desc, link, github], i) => (
+        <div
+          className="flex flex-col border rounded-2xl p-5 border-secondary gap-4 transition hover:-translate-y-1 cursor-pointer"
+          key={i}
+        >
+          <Image
+            src={preview}
+            alt={preview}
+            width={100}
+            height={50}
+            className="w-full"
+          />
+          <div>
+            <div className="text-xl font-semibold">{title}</div>
+            <p>{desc}</p>
+          </div>
+          <div className="flex pr-5 gap-2 text-white">
+            <a
+              className="flex gap-1 p-1 bg-[#2C3E50] transition hover:text-blue-400 rounded-lg"
+              href={link}
+              target="_blank"
+            >
+              <Link />
+              Link
+            </a>
+            <a
+              className="flex gap-2 p-1 bg-[#333333] transition hover:text-blue-400 rounded-lg"
+              href={github}
+              target="_blank"
+            >
+              <Github />
+              Github
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecentProjects;
