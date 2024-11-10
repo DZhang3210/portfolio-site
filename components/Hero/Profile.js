990b6a5cc0f42f43c21286203a550b5ad770f7cf
DZhang3210 <53838139+DZhang3210@users.noreply.github.com>
2024-11-10 12:59:25 -0500

    new commit

--- components/Hero/Profile.js
+++ components/Hero/Profile.js
@@ -5,7 +5,7 @@ const Profile = () => {
   return (
     <div className="flex flex-col gap-5 items-start my-0" id="portfolio">
       <div className="text-5xl font-semibold text-gray-600">My Portfolio</div>
-      <hr />
+      <hr className="w-full" />
 
       <RecentProjects />
     </div>
